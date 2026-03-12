f7966a11f839b526b68ba8a5b3a9be7b28185cf2
mxvsh <hello@monawwar.io>
2026-03-12 15:05:52 +0530

    Add Hugeicons to the icon sets in IconPickerModal

--- components/icon-picker/IconPickerModal.tsx
+++ components/icon-picker/IconPickerModal.tsx
@@ -14,6 +14,7 @@ import { IconGrid } from "./IconGrid";
 
 const ICON_SETS = [
   { id: "lucide", label: "Lucide" },
+  { id: "hugeicons", label: "Hugeicons" },
   { id: "heroicons", label: "Heroicons" },
   { id: "ph", label: "Phosphor" },
   { id: "ri", label: "Remix Icons" },
